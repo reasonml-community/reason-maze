122d990ad394cc3371c8f4baeee35e30404ed775
Jared Forsyth <jared@jaredforsyth.com>
2017-06-14 23:16:10 -0600

    add public

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './lib/js/src/main.js',
   output: {
-    path: path.join(__dirname, "bundledOutputs"),
-    filename: '[name].js',
+    path: path.join(__dirname, "public"),
+    filename: 'bundle.js',
   },
 };
